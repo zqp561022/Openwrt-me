5cd5a86d27af777d012172907bb6ed5882777413
flykky <40863329+zqp561022@users.noreply.github.com>
2023-05-08 09:14:16 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -30,18 +30,21 @@ git clone https://github.com/gngpp/luci-theme-neobird.git package/luci-theme-neo
 # git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 # git clone https://github.com/firkerword/openwrt-mos.git package/openwrt-mos
-git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+# git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+git clone https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 # git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone https://github.com/firkerword/luci-app-mosdns.git package/mosdns
-git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
-git clone https://github.com/firkerword/smartdns.git package/smartdns
+# git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+# git clone https://github.com/firkerword/smartdns.git package/smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git package/openwrt-smartdns
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 # git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
-git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
+# git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
 # git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
