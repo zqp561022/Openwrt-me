82a5d39a863d4027278e9995d5c80f186bfe2907
flykky <40863329+zqp561022@users.noreply.github.com>
2024-04-02 20:28:46 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.11/g' package/base-files/files/bin/config_generate
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
-sed -i 's/KERNEL_PATCHVER:=6.2/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 function merge_package(){
     repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
