2dff7c45997046ced9753ddf877e9837dcde07fe
flykky <40863329+zqp561022@users.noreply.github.com>
2024-04-06 16:20:29 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -12,6 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.11/g' package/base-files/files/bin/config_generate
+
+# 修改内核版本
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
